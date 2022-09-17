e55ac2e73f79d0067a9f8488dd4d72b85f5cdc68
Simiao Huang <hsmsyouxiang@163.com>
2022-09-17 23:39:03 +0930

    Delete useless import

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import logo from './logo.png';
+
 //import PrimarySearchAppBar from './Menu'
 
 
