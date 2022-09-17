2a739e62886e91a18738a87814d65a964edf6bb1
Simiao Huang <hsmsyouxiang@163.com>
2022-09-18 00:02:09 +0930

    add untracked files

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,22 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "app1": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "HostedUIDomain": "string",
+            "OAuthMetadata": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string",
+            "CreatedSNSRole": "string"
+        }
+    },
+    "storage": {
+        "s3app1storage04cf2aaf": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
